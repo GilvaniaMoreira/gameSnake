f69678eef044e2eaba4a4c44b540adfd92faa890
GilvaniaMoreira <moreiragil84@gmail.com>
2022-06-02 17:46:55 -0300

    definir direções e que a cobra suma

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 let canvas = document.getElementById("snake");
 let context = canvas.getContext("2d");
 let box = 32;
-let snake = []
+let snake = [];
 snake[0] = {
     x: 8 * box,
     y: 8 * box
@@ -21,7 +21,23 @@ function criarCobrinha() {
     }
 }
 
+document.addEventListener('keydown', update);
+
+function update (event) {
+    if (event.keyCode == 37 && direction != 'right') direction = 'left';
+    if (event.keyCode == 38 && direction != 'down') direction = 'up';
+    if (event.keyCode == 39 && direction != 'left') direction = 'right';
+    if (event.keyCode == 40 && direction != 'up') direction = 'down';
+    
+    
+}
+
 function iniciarJogo() {
+    if(snake[0].x > 15 * box && direction == 'rigth') snake[0].x = 0;
+    if(snake[0].x < 0 && direction == 'left') snake[0].x = 16 * box;
+    if(snake[0].y > 15 * box && direction == 'down') snake[0].y = 0;
+    if(snake[0].y < 0 && direction == 'up') snake[0].y = 16 * box;
+
     criarBG();
     criarCobrinha();
 
@@ -31,18 +47,18 @@ function iniciarJogo() {
     if (direction == 'right') snakeX += box;
     if (direction == 'left') snakeX -= box;
     if (direction == 'up') snakeY -= box;
-    if (direction == 'down') snakeY =+ box;
+    if (direction == 'down') snakeY += box;
 
     snake.pop();
 
     let newHead = {
-        x: snakex,
-        y: snakey
+        x: snakeX,
+        y: snakeY
     }
 
     snake.unshift(newHead);
 }
 
-let jogo = settInterval(iniciarJogo, 100)
+let jogo = setInterval(iniciarJogo, 100);
 
 
